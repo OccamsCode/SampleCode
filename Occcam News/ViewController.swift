7d02da7fa3942d9dd5b110ff07ee1799cae3701a
b.munjoma <apophis@TouchBar-MacBrian-Pro.local>
2021-02-15 12:20:10 +0000

    Initial commit.

---
+++ Occcam News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 15/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
