2a3811af3c0e6dba69a73006765d82c7f975702a
b.munjoma <apophis@TouchBar-MacBrian-Pro.local>
2021-02-26 13:47:16 +0000

    Added secondary cell

---
+++ Occcam News/News Flow/CollectionView Cells/ArticleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleCollectionViewCell.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 26/02/2021.
+//
+
+import UIKit
+
+class ArticleCollectionViewCell: UICollectionViewCell {
+    
+}
