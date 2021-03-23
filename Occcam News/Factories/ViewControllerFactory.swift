3c96b99effa1dccf4e42d6f0505440cf418ed720
b.munjoma <apophis@TouchBar-MacBrian-Pro.local>
2021-03-23 16:39:01 +0000

    Added a simple view controller factory

---
+++ Occcam News/Factories/ViewControllerFactory.swift
@@ -0,0 +1,23 @@
+//
+//  ViewControllerFactory.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 23/03/2021.
+//
+
+import UIKit
+import Foundation
+import SafariServices
+
+class ViewControllerFactory {
+    
+    // Produce a SFSafariViewController
+    class func produce(safariControllerFrom article: Article) -> SFSafariViewController {
+        return SFSafariViewController(url: article.url)
+    }
+    
+    // Produce a generic ViewController which is IBInstantiatable
+    class func produce<T: UIViewController>(_ viewController: T.Type) -> T where T: IBInstantiatable {
+        return viewController.instantiate()
+    }
+}
