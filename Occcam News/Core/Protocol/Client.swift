b919ee00060cc94ff3f8e56ed63c1a6c626ffcad
Brian M <b.a.munjoma@gmail.com>
2023-08-21 15:25:13 +0100

    Refactored Client

--- Occcam News/Core/Protocol/Client.swift
+++ Occcam News/Core/Protocol/Client.swift
@@ -26,21 +26,22 @@ extension URLSession: URLSessionType {
 }
 
 // swiftlint:disable empty_enum_arguments
+// swiftlint:disable line_length
 enum APIError: Error, Equatable {
 
     case invalidData
     case invalidResponse
-    case invalidResponseStatusCode
+    case unhandledStatusCode(code: Int)
     case response(error: Error)
     case decode(error: Error)
 
     var localizedDescription: String {
         switch self {
-        case .invalidData: return "Invalid Data"                                // No data sent
-        case .invalidResponse: return "Invalid Response"                        // URLResponse not HTTPURLResponse
-        case .invalidResponseStatusCode: return "Invalid Response StatusCode"   // Status Code not 2xx
-        case .response(let error): return "Response Error \(error)"             // Error from Request
-        case .decode(let error): return "Decode Error \(error)"                 // Error from Decoder
+        case .invalidData: return "Invalid Data"                                            // No data sent
+        case .invalidResponse: return "Invalid Response"                                    // URLResponse not HTTPURLResponse
+        case .unhandledStatusCode(let code): return "Invalid Response StatusCode \(code)"   // Status Code not 2xx
+        case .response(let error): return "Response Error \(error)"                         // Error from Request
+        case .decode(let error): return "Decode Error \(error)"                             // Error from Decoder
         }
     }
 
@@ -48,7 +49,7 @@ enum APIError: Error, Equatable {
         switch (lhs, rhs) {
         case (.invalidData, .invalidData),
             (.invalidResponse, .invalidResponse),
-            (.invalidResponseStatusCode, .invalidResponseStatusCode),
+            (.unhandledStatusCode(_), .unhandledStatusCode(_)),
             (.response(_), .response(_)),
             (.decode(_), .decode(_)):
             return true
@@ -57,6 +58,7 @@ enum APIError: Error, Equatable {
     }
 }
 // swiftlint:enable empty_enum_arguments
+// swiftlint:enable line_length
 
 protocol Client {
 
@@ -65,4 +67,41 @@ protocol Client {
 
     func dataTask<T>(with resource: Resource<T>,
                      completion: @escaping (Result<T, APIError>) -> Void ) -> URLSessionTaskType? where T: Decodable
+}
+
+extension Client {
+    func dataTask<T>(with resource: Resource<T>,
+                     completion: @escaping (Result<T, APIError>) -> Void ) -> URLSessionTaskType? {
+
+        Log.verbose(resource.request)
+
+        guard let urlReqest = URLRequest(request: resource.request,
+                                         in: environment) else { return nil }
+
+        let task = urlSession.dataTask(with: urlReqest) { data, response, error in
+
+            if let error = error {
+                return completion(.failure(.response(error: error)))
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                return completion(.failure(.invalidResponse))
+            }
+
+            switch httpResponse.statusCode {
+            case 200...299:
+                guard let data = data else { return completion(.failure(.invalidData)) }
+                do {
+                    let decoded = try resource.decode(data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(.decode(error: error)))
+                }
+            default:
+                completion(.failure(.unhandledStatusCode(code: httpResponse.statusCode)))
+            }
+
+        }
+        return task
+    }
 }
