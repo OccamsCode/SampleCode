9b4ff226e9ac3e67cd0918e1bd778f21760a4419
Brian M <b.a.munjoma@gmail.com>
2023-11-13 17:46:26 +0000

    Hello SwiftUI

---
+++ Occcam News/OCNApp.swift
@@ -0,0 +1,23 @@
+//
+//  OCNApp.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World 🌍")
+    }
+}
