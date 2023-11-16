3475e1c52258cafb8f80f7dcec6d309a5bdc30c8
Brian M <b.a.munjoma@gmail.com>
2023-11-16 13:17:31 +0000

    Create SafariView

---
+++ Occcam News/Features/TopHeadlines/SafariView.swift
@@ -0,0 +1,20 @@
+//
+//  SafariView.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 16/11/2023.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: Context) -> some SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+}
