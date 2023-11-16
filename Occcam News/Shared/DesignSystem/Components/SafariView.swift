3423037f2a2b70bceb5a3f106aa8c629b305a431
Brian M <b.a.munjoma@gmail.com>
2023-11-16 16:31:46 +0000

    Create ShareView/UIActivityViewController

--- Occcam News/Shared/DesignSystem/Components/SafariView.swift
+++ Occcam News/Shared/DesignSystem/Components/SafariView.swift
@@ -9,12 +9,11 @@ import SwiftUI
 import SafariServices
 
 struct SafariView: UIViewControllerRepresentable {
-    
     let url: URL
-    
+
     func makeUIViewController(context: Context) -> some SFSafariViewController {
         return SFSafariViewController(url: url)
     }
-    
+
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
 }
