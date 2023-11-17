56b5c92cb6dab12bf765e063266d6272daf162d4
Brian M <b.a.munjoma@gmail.com>
2023-11-17 21:29:20 +0000

    Create News Category

---
+++ Occcam News/Core/Network/Client/NewsCategory.swift
@@ -0,0 +1,18 @@
+//
+//  NewsCategory.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 17/11/2023.
+//
+
+enum NewsCategory: String, CaseIterable {
+    case general
+    case world
+    case nation
+    case business
+    case technology
+    case entertainment
+    case sports
+    case science
+    case health
+}
