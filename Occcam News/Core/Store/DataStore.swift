cbadbc3d33c6bebd7b826f68db972a6886daf39b
Brian M <b.a.munjoma@gmail.com>
2023-11-27 16:43:50 +0000

    Create basic Data Store

---
+++ Occcam News/Core/Store/DataStore.swift
@@ -0,0 +1,58 @@
+//
+//  DataStore.swift
+//  Occcam News
+//
+//  Created by Brian Munjoma on 27/11/2023.
+//
+
+import Foundation
+
+protocol Store: Actor {
+    
+    associatedtype Value
+    
+    func save(_ value: Value)
+    func load() -> Value?
+}
+
+actor PlistStore<T: Codable>: Store where T: Equatable {
+    
+    var saved: T?
+    let filename: String
+    
+    init(_ filename: String) {
+        self.filename = filename
+    }
+    
+    private var url: URL {
+        FileManager.default
+            .urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent("\(filename).plist")
+    }
+    
+    func save(_ value: T) {
+        if let saved = saved, saved == value { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            encoder.outputFormat = .binary
+            let data = try encoder.encode(value)
+            try data.write(to: url, options: [.atomic])
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func load() -> T? {
+        do {
+           let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            let value = try decoder.decode(T.self, from: data)
+            saved = value
+            return value
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
