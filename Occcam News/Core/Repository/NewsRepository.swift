8358e58ab81663aa42659730f04217302ac91063
Brian M <b.a.munjoma@gmail.com>
2023-11-29 16:06:32 +0000

    Create SearchNewsRepository

--- Occcam News/Core/Repository/NewsRepository.swift
+++ Occcam News/Core/Repository/NewsRepository.swift
@@ -13,6 +13,10 @@ protocol TopHeadlinesRepository {
     func fetchTopHeadlines(inCategory: NewsCategory) async throws -> Result<[Article], RequestError>
 }
 
+protocol SearchNewsRepository {
+    func search(for query: String) async throws -> Result<[Article], RequestError>
+}
+
 final class NewsRepository {
     @Injected(\.clientProvider) var client
 }
@@ -20,6 +24,22 @@ final class NewsRepository {
 extension NewsRepository: TopHeadlinesRepository {
     func fetchTopHeadlines(inCategory category: NewsCategory) async throws -> Result<[Article], RequestError> {
         let request = TopHeadlinesRequest(category)
+        let resource = Resource<RemoteHeadlines>(request: request)
+        let result = await client.executeRequest(with: resource)
+
+        switch result {
+        case .success(let success):
+            let (_, articles) = DomainMapper.map(success)
+            return .success(articles)
+        case .failure(let failure):
+            return .failure(failure)
+        }
+    }
+}
+
+extension NewsRepository: SearchNewsRepository {
+    func search(for query: String) async throws -> Result<[Article], RequestError> {
+        let request = SearchRequest(query)
         let resource = Resource<RemoteHeadlines>(request: request)
         let result = await client.executeRequest(with: resource)
 
