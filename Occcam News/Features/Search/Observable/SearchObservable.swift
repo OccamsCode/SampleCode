af8f16ab30ad0993768a99e80481366992b46dca
Brian M <b.a.munjoma@gmail.com>
2023-11-30 16:56:00 +0000

    Create functions to add/remove a history item

--- Occcam News/Features/Search/Observable/SearchObservable.swift
+++ Occcam News/Features/Search/Observable/SearchObservable.swift
@@ -13,24 +13,29 @@ class SearchObservable: ObservableObject, LoadableObject {
     @Published private(set) var phase: LoadingState<[Article]>
     @Published var searchTerm: String
     @Published var searchHistoryTerms: [String] = []
+    
+    private let maximumSearchHistoryLimit: Int
 
     internal init(repository: SearchNewsRepository,
                   phase: LoadingState<[Article]> = .idle,
-                  searchTerm: String = "") {
+                  searchTerm: String = "",
+                  maximumSearchHistoryLimit: Int = 10) {
         self.repository = repository
         self.phase = phase
         self.searchTerm = searchTerm
+        self.maximumSearchHistoryLimit = maximumSearchHistoryLimit
     }
 
     @MainActor
     func searchArticles() async {
 
+        if Task.isCancelled { return }
         let searchQuery = searchTerm.trimmingCharacters(in: .whitespacesAndNewlines)
         if searchQuery.isEmpty { return }
         phase = .loading
         do {
             let result = try await repository.search(for: searchQuery)
-
+            if Task.isCancelled { return }
             switch result {
             case .success(let success):
                 phase = .success(success)
@@ -38,6 +43,7 @@ class SearchObservable: ObservableObject, LoadableObject {
                 phase = .failure(failure)
             }
         } catch {
+            if Task.isCancelled { return }
             phase = .failure(error)
         }
     }
@@ -45,6 +51,24 @@ class SearchObservable: ObservableObject, LoadableObject {
     func load() {
         Task {
             await searchArticles()
+        }
+    }
+    
+    func addHistory(_ newSearchTerm: String) {
+        if let index = searchHistoryTerms.firstIndex(where: {
+            newSearchTerm.caseInsensitiveCompare($0) == .orderedSame
+        }) {
+            searchHistoryTerms.remove(at: index)
+        } else if searchHistoryTerms.count == maximumSearchHistoryLimit {
+            searchHistoryTerms.removeLast()
+        }
+
+        searchHistoryTerms.insert(newSearchTerm, at: searchHistoryTerms.startIndex)
+    }
+
+    func removeHistory(_ newSearchTerm: String) {
+        searchHistoryTerms.removeAll {
+            newSearchTerm.caseInsensitiveCompare($0) == .orderedSame
         }
     }
 }
