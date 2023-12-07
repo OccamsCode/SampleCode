1d3d4826949e37784ec1904933c1a7b91f9eece5
Brian M <b.a.munjoma@gmail.com>
2023-12-07 16:45:05 +0000

    Using a UUID for the preview Article

--- Occcam News/Core/Domain/Models/Article.swift
+++ Occcam News/Core/Domain/Models/Article.swift
@@ -28,7 +28,7 @@ extension Article: Codable {}
 // swiftlint: disable line_length
 extension Article: Previewable {
     static var preview: Article {
-        return Article(title: "NASA and ISRO to launch NISAR mission in 2024; know all about it",
+        return Article(title: UUID().uuidString,
                        description: "Know all about NISAR mission tests and the expected launch date from NASA NISAR Project Manager Phil Barela. It is a joint NASA and ISRO project.",
                        url: URL(string: "https://bc.ctvnews.ca/b-c-to-test-emergency-alert-system-wednesday-1.6644970")!,
                        image: URL(string: "https://www.svtstatic.se/image-news/1200/1.91:1/0.5/0.5/c5d6c2228b01106b63e91e67f80d5ecab2abcf0f384051dd8cf59eb39910ab09"),
